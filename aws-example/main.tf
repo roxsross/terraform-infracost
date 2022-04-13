6ca6f4664ae2f29e8abf3a044412bd78da7290e9
Rossana Suarez <rossanadelvalle.suarez@naranjax.com>
2022-04-13 09:35:25 -0300

    update

--- aws-example/main.tf
+++ aws-example/main.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region                      = "us-east-1" # <<<<< Try changing this to eu-west-1 to compare the costs
   skip_credentials_validation = true
   skip_requesting_account_id  = true
-  profile                     = roxsross
-  shared_credentials_file     = "~/.aws/credentials"
 }
 
 resource "aws_instance" "web_app" {
@@ -11,14 +9,15 @@ resource "aws_instance" "web_app" {
   instance_type = "t2.micro"              # <<<<< Try changing this to m5.8xlarge to compare the costs
 
   root_block_device {
-    volume_size = 50
+    volume_size = 20
   }
 }
 
-resource "aws_lambda_function" "hello_world" {
-  function_name = "hello_world"
-  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
-  handler       = "exports.test"
-  runtime       = "nodejs12.x"
-  memory_size   = 1024                      # <<<<< Try changing this to 512 to compare costs
+resource "aws_s3_bucket" "demobucket" {
+  bucket = "bucket-infracost-v9"
+
+  tags = {
+    Name        = "roxsross"
+    Environment = "Dev"
+  }
 }
